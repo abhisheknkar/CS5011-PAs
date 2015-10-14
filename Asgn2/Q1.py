d062a64bcbb39117077482039be2268c6e616265
abhishek <abhisheknkar@gmail.com>
2015-10-14 20:32:46 +0530

    Reading Data

--- Asgn2/Q1.py
+++ Asgn2/Q1.py
@@ -2,22 +2,58 @@ __author__ = 'abhishek'
 
 import numpy as np
 from os import listdir
-
+import pickle
+from skimage import io
 
 def output_activation(x,deriv=False):
     #Sigmoid
     1
 
+def savepickle(var,filename):
+    output = open(filename, 'wb')
+    pickle.dump(var, output)
+    output.close()
+
+def loadpickle(filename):
+    pkl_file = open(filename, 'rb')
+    output = pickle.load(pkl_file)
+    pkl_file.close()
+    return output
+
 def loadImageData(folder):
+    count = 0
     subfolders = ['coast','forest','insidecity','mountain']
     modes = ['Test','Train']
+    bins = 32
+    Xtrain = np.zeros((1,3*bins))
+    Ytrain = np.zeros((1,1))
+    Xtest = np.zeros((1,3*bins))
+    Ytest = np.zeros((1,1))
+    hist = [[] for i in range(3)]
+
     for subfolder in subfolders:
         for mode in modes:
-        filenames = listdir(folder+subfolder)
-        print filenames
+            prefix = folder+subfolder+'/'+mode+'/'
+            filenames = listdir(prefix)
+            for filename in filenames:
+                I = io.imread(prefix+filename)
+                features = []
+                for layer in range(3):
+                    [hist[layer] ,temp] = np.histogram(I[:,:,layer],np.arange(bins+1))
+                    features += hist[layer].tolist()
 
-    return (1,1)
+                if mode == 'Train':
+                    Xtrain = np.vstack((Xtrain,np.asarray(features)))
+                    Ytrain = np.vstack((Ytrain,subfolders.index(subfolder)))
+                else:
+                    Xtest = np.vstack((Xtest,np.asarray(features)))
+                    Ytest = np.vstack((Ytest,subfolders.index(subfolder)))
+                print count
+                count+=1
+    return (Xtrain[1:,:],Ytrain[1:],Xtest[1:,:],Ytest[1:])
 
 
 if __name__ == '__main__':
-    (Xdata,Ydata) = loadImageData('../Asgn1/data/DS4/data_students/')
+    (Xtrain,Ytrain,Xtest,Ytest) = loadImageData('../Asgn1/data/DS4/data_students/')
+    savepickle((Xtrain,Ytrain,Xtest,Ytest),'Q1Out/data.pkl')
+    (Xtrain,Ytrain,Xtest,Ytest) = loadpickle('Q1Out/data.pkl')
