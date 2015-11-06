b64a54f75954cfc8883b8e8b5995ccfd739149af
abhishek <abhisheknkar@gmail.com>
2015-11-06 13:28:41 +0530

    Hierarchical clustering outputs added for QA5

--- Asgn2/stdnormalize.m
+++
@@ -1,5 +0,0 @@
-function [ in, mu, sigma ] = stdnormarlize( in )
-    mu = mean(in,1);
-    sigma = std(in,1);
-    in = (in - repmat(mu,size(in,1),1)) ./ repmat(sigma,size(in,1),1);
-end
--- Asgn3/QA5.py
+++ Asgn3/QA5.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     getBatchPurity("/home/csd154server/QA5Out_Spiral_DBSCAN/")
     getBatchPurity("/home/csd154server/QA5Out_Path-based_DBSCAN/")
     getBatchPurity("/home/csd154server/QA5Out_Flames_DBSCAN/")
+    getBatchPurity("QA5Out_Hierarchical/")
 
 '''
 Spiral:
@@ -26,8 +27,8 @@ Flames:
         Ward is the clear winner, perfect accuracy
 Path-based:
     DBSCAN:
-        Extreme: minpts=1,eps=0.05,clusters=30.arff: purity = 1.0
-        Best: minpts=13,eps=0.1,clusters=2.arff: purity = 0.626666666667
+        Extreme: minpts=7,eps=0.05,clusters=30.arff: purity = 1.0
+        Best: minpts=7,eps=0.05,clusters=3.arff: purity = 0.53333333
     Hierarchical:
         Ward does the best, but isn't a clear winner
 '''
